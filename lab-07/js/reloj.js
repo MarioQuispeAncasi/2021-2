35242c9f352cf28a486cc4d3f0d768991dc2feda
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2021-09-29 20:08:37 -0500

    .

---
+++ lab-07/js/reloj.js
@@ -0,0 +1,8 @@
+function mostrarReloj(){
+    var fechahora=new Date();
+    var hora = fechahora.getHours();
+    var minutos = fechahora.getMinutes();
+    var minutos = fechahora.getMinutes();
+    var segundos = fechahora.getSeconds();
+    
+}
