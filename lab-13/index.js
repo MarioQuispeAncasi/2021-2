0b56b520168c9a060617242aa9c2815c395f4df3
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2021-10-27 21:02:04 -0500

    .

--- lab-13/index.js
+++ lab-13/index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/*const http = require('http')
 
 const hostname = 'localhost'
 
@@ -9,7 +9,7 @@ const hostname = 'localhost'
 4. Definir las rutas
 5. Conectar a bd
 6. Inciar al app/ servidor
-*/
+
 //Declarar los modulos y dependencias
 
 //modulos
@@ -39,7 +39,7 @@ app.set("view engine", "ejs"); //definir el motor de busquedas
 app.set("views",__dirname + "/views");
 app.set("layout", "../layout/´plantilla1");
 /*la vista es la data que será insertada dentro 
-de la plantilla o layout*/
+de la plantilla o layout
 
 //Definir los middleware, se ejecutan en orden
 app.use(ejslayout);
@@ -66,3 +66,68 @@ app.use("/login", r1);
 const server = http.createServer(app);
 server.listen(app.get("port"), hostname,()=>{
     console.log(`Servidor iniciado en puerto ... ${app.get('port')}`);})
+*/
+const http = require('http')
+
+const hostname = 'localhost'
+
+/*
+La reglas:
+1. Incluir modulos y depencias
+2. COnfigurar express con el motor de plantillas
+3. Definir los middleware
+4. Definicion de rutas
+5. COnectar a BD
+6. Iniciar al app / servidor
+*/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const favicon = require("serve-favicon");
+const session = require("express-session");
+const ejsLayout = require("express-ejs-layouts");
+
+// Dependencias
+const r1 = require("./routes/alumno1.js");
+
+// instanciar Express
+const app = express();
+app.set('port', process.env.port || 3000);
+app.set('env','development'); // test stage preview production
+
+// Motor de plantillas
+app.set('view cache', "false");
+app.set("view engine", "ejs");
+app.set("views",__dirname + "/views");
+app.set("layout","../layouts/plantilla1");
+
+// Middleware
+app.use(ejsLayout);
+app.use( favicon( __dirname + "/public/favicon.ico") );
+app.use( morgan("combined") );
+
+app.use( cookieParser() );
+app.use( session( {
+    secret : "frase clave",
+    resave : false,
+    cookie : { secure: false }
+}))
+
+app.use( express.static( __dirname + "/public"));
+
+// La aplicacion debe estar en ...
+// Esta parte la hizo el alumno 1
+app.use("/login", r1);
+// Esta parte la hizo el alumno 2
+
+
+
+// Instanciar el server
+const server = http.createServer(app);
+server.listen( app.get('port') , hostname , () => {
+    console.log(`Servidor iniciado en puerto ... ${app.get('port')}`);
+
+})
+
--- lab-13/routes/alumno1.js
+++ lab-13/routes/alumno1.js
@@ -1,19 +1,15 @@
-// Instanciar a Express
 const express = require("express");
-const ruta = new express.Router();
 
+const ruta = express.Router();
 
 // Setear a express
 ruta.use( express.urlencoded( {extended: true }) )
 ruta.use( express.json() )
 
-// Otra forma de leer los archivos estaticos
-ruta.use( express.static( __dirname + '/public'))
-
 //----- DEFINICION DE RUTAS
 ruta.route('/')
     .post( (req,res,next) => {
-    res.end("Recibi un POST y obtuve " + req.body.codigo + " " + req.body.nombre);
+       res.end("Recibi un POST y obtuve " + req.body.codigo + " " + req.body.nombre);
 }) 
 
 ruta.route('/')
@@ -24,7 +20,6 @@ ruta.route('/')
 ruta.route('/:codigo/:nombre')
     .get( (req,res,next) => {
     res.end("Recibi un GET v2 y obtuve " + req.params.codigo + " " + req.params.nombre);
-
 })
 
 ruta.route("/")
@@ -34,11 +29,11 @@ ruta.route("/")
 })
 
 ruta.route("/")
-.delete( (req,res,next) => {
+    .delete( (req,res,next) => {
     res.statusCode = 405
     res.end("Metodo no implementado aun ...")
 })
 
 
-//No olvidar que hay que exportar
+// NO OLVIDAR QUE HAY QUE EXPORTAR
 module.exports = ruta;
