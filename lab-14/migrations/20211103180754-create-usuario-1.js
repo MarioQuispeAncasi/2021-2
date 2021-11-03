e0c96b7b2d221a1113e6e2b32630d59cdf2cbbb1
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2021-11-03 13:11:14 -0500

    .

--- lab-14/migrations/20211103180754-create-usuario-1.js
+++ lab-14/migrations/20211103180754-create-usuario-1.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Usuarios', {
+    await queryInterface.createTable('Usuario1s', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Usuarios');
+    await queryInterface.dropTable('Usuario1s');
   }
 };
--- lab-14/models/usuario1.js
+++ lab-14/models/usuario1.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Usuario extends Model {
+  class Usuario1 extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  Usuario.init({
+  Usuario1.init({
     nombre: DataTypes.STRING,
     codigo: DataTypes.STRING,
     edad: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Usuario',
+    modelName: 'Usuario1',
   });
-  return Usuario;
+  return Usuario1;
 };
