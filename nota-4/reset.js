fe4f1e97499f7822f4a531fc88d76de8948c382c
MarioQuispeA <20181529@aloe.ulima.edu.pe>
2021-11-12 21:32:28 -0500

    .

--- nota-4/reset.js
+++ nota-4/reset.js
@@ -1,3 +1,10 @@
+/******************************************
+ *  P R A C T I C A    C A L I F I C A D A
+ *  Nota #4
+ * 
+ * Nombre :  Mario Daniel Quispe Ancasi
+ * Codigo :  20181529
+ ******************************************/
 const db = require("./models");
 
 //db.sequelize.sync();
